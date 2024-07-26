9f54e7d9814eab80991242b1c71acf9dd6fcf318
Ruslan Prusakov <ruslan.prusakov@gmail.com>
2024-07-27 00:37:07 +0300

    catalog images

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -5,9 +5,10 @@ import style from './catalog.module.css'
 const categories = ['Межкомнатные двери', 'Входные двери', 'Замки и ручки', 'Противопожарные двери']
 
 export default function Catalog() {
+  
   return(
     <div className={`container ${style.catalog_container}`}> 
-      <h1>Каталог</h1>
+      <h1 className="section-title">Каталог</h1>
       <div className={style.box_list}>
         <div className={style.box_row}>
           <div className={style.box}>
@@ -16,6 +17,7 @@ export default function Catalog() {
                 <h4 className={style.category_name}>Межкомнатные двери</h4>
               </div>
               <div className={style.category_image}>
+              {/* ./categ_img/doors.jpg */}
                 <img className={style.image} src="./categ_img/doors.jpg" alt="" />
               </div>
             </a>
@@ -26,6 +28,7 @@ export default function Catalog() {
                 <h4 className={style.category_name}>Межкомнатные двери</h4>
               </div>
               <div className={style.category_image}>
+              {/* ./categ_img/fire_door.jpg */}
                 <img className={style.image} src="./categ_img/fire_door.jpg" alt="" />
               </div>
             </a>
@@ -36,6 +39,7 @@ export default function Catalog() {
                 <h4 className={style.category_name}>Межкомнатные двери</h4>
               </div>
               <div className={style.category_image}>
+              {/* ./categ_img/invis_door.jpeg */}
                 <img className={style.image} src="./categ_img/invis_door.jpeg" alt="" />
               </div>
             </a>
@@ -48,6 +52,7 @@ export default function Catalog() {
                 <h4 className={style.category_name}>Межкомнатные двери</h4>
               </div>
               <div className={style.category_image}>
+              {/* ./categ_img/open_door.jpg */}
                 <img className={style.image} src="./categ_img/open_door.jpg" alt="" />
               </div>
             </a>
@@ -58,6 +63,7 @@ export default function Catalog() {
                 <h4 className={style.category_name}>Межкомнатные двери</h4>
               </div>
               <div className={style.category_image}>
+              {/* ./categ_img/ruchki.jpg */}
                 <img className={style.image} src="./categ_img/ruchki.jpg" alt="" />
               </div>
             </a>
